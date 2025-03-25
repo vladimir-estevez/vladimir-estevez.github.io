017f659bd59dec4c6bd2ed7471c38a8d9dcf5be4
vladimir-estevez <jvestev@gmail.com>
2025-03-25 15:25:57 -0400

    manifest.json and VR blackjack changes

--- vladimir-estevez-portfolio/src/components/projects/blackjack.js
+++ vladimir-estevez-portfolio/src/components/projects/blackjack.js
@@ -17,9 +17,7 @@ export const blackjack = {
     'The primary goal of this project was to develop a fully immersive VR Blackjack game tailored for the Meta Quest 2 VR headset, ' +
     'while gaining expertise in Unity3D development, VR hardware optimization, and Unity VR game builds.\n\n' +
     'The game replicates the ambiance of a real-world casino blackjack table, allowing players to interact with betting mechanics ' +
-    'in a realistic 3D environment.\n\nCore features include:\n\n' +
-    '• Intuitive VR controls for smooth gameplay.\n\n' +
-    '• A robust betting system for realistic wagering.\n\n' +
+    'in a realistic 3D environment.\n\n' + 'Core features include intuitive VR controls for smooth gameplay, a robust betting system for realistic wagering, and meticulously designed scenarios that simulate the dynamic atmosphere of a casino.\n\n' + 
     'A major challenge was working with 3D visual assets, as we had no prior experience in this area. ' +
     'We learned to integrate and optimize 3D models, textures, and animations within Unity to create a polished and immersive experience.\n\n' +
     'Additionally, we focused on user experience design, game mechanics, and performance optimization for VR hardware.\n\n' +
